60d178c50582f5f9c7c787d962f80f5b6638a5b3
Erika-Petty <61434658+Erika-Petty@users.noreply.github.com>
2020-04-03 16:22:05 -0600

    Add files via upload

--- Project.m
+++ Project.m
@@ -1,17 +1,7 @@
 % Comp Methods Project 
 
-%% 
-clear, clc
-
-test = license('test','image_toolbox');
-if test == 0
-    disp('To run this project you must have the Image Processing Toolbox. It looks like you do not have it and you should please install it before using this program.')
-else
-    disp('The test to check if you have the Image Processing Toolbox is approved.')
-end
-        
 %% Loading Data into Matlab
-
+clear, clc
 
 % stores information about the MRI images into a structure
 info1 = dicominfo('IM-0001-0001.dcm');
@@ -24,4 +14,41 @@ mri2 = dicomread(info2);
 mri3 = dicomread(info3);
 mri4 = dicomread(info4);
 
+figure
+subplot(2,2,1)
+imshow(mri1,[])
+title('MRI Image 1')
 
+subplot(2,2,2)
+imshow(mri2,[])
+title('MRI Image 2')
+
+subplot(2,2,3)
+imshow(mri3,[])
+title('MRI Image 3')
+
+subplot(2,2,4)
+imshow(mri4,[])
+title('MRI Image 4')
+%% Thresholding
+
+figure
+subplot(2,2,1)
+j_1 = imadjustn(mri1);
+imshow(j_1, [])
+title('MRI Image 1 - Thresholding')
+
+subplot(2,2,2)
+j_2 = imadjustn(mri2);
+imshow(j_2, [])
+title('MRI Image 2 - Thresholding')
+
+subplot(2,2,3)
+j_3 = imadjustn(mri3);
+imshow(j_3, [])
+title('MRI Image 2 - Thresholding')
+
+subplot(2,2,4)
+j_4 = imadjustn(mri4);
+imshow(j_4, [])
+title('MRI Image 2 - Thresholding')
