7b2dac1b5a226609dc9a550500a71301d2477b0b
shellybaltz <61434768+shellybaltz@users.noreply.github.com>
2020-04-03 16:14:40 -0600

    Add files via upload
    I added a check to see if the user has the image processing toolbox

--- Project.m
+++ Project.m
@@ -1,7 +1,17 @@
 % Comp Methods Project 
 
-%% Loading Data into Matlab
+%% 
 clear, clc
+
+test = license('test','image_toolbox');
+if test == 0
+    disp('To run this project you must have the Image Processing Toolbox. It looks like you do not have it and you should please install it before using this program.')
+else
+    disp('The test to check if you have the Image Processing Toolbox is approved.')
+end
+        
+%% Loading Data into Matlab
+
 
 % stores information about the MRI images into a structure
 info1 = dicominfo('IM-0001-0001.dcm');
