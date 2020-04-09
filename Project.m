f174a899481a91390389e3b7d70aa9f5122a7148
Erika-Petty <61434658+Erika-Petty@users.noreply.github.com>
2020-04-08 19:35:49 -0600

    Add files via upload

--- Project.m
+++ Project.m
@@ -24,69 +24,34 @@ mri2 = dicomread(info2);
 mri3 = dicomread(info3);
 mri4 = dicomread(info4);
 
-% figure
-% subplot(2,2,1)
-% imshow(mri1,[])
-% title('MRI Image 1')
-% 
-% subplot(2,2,2)
-% imshow(mri2,[])
-% title('MRI Image 2')
-% 
-% subplot(2,2,3)
-% imshow(mri3,[])
-% title('MRI Image 3')
-% 
-% subplot(2,2,4)
-% imshow(mri4,[])
-% title('MRI Image 4')
-
-
 figure
-Matrix = cat(3, mri1, mri2, mri3, mri4);
-s = smooth3(Matrix);
-fv = patch(isosurface(s, 'isovalue'))
+subplot(2,2,1)
+imshow(mri1,[])
+title('MRI Image 1')
+ 
+subplot(2,2,2)
+imshow(mri2,[])
+title('MRI Image 2')
 
+subplot(2,2,3)
+imshow(mri3,[])
+title('MRI Image 3')
+ 
+subplot(2,2,4)
+imshow(mri4,[])
+title('MRI Image 4')
 
 %% Thresholding and Selecting ROI
 
 figure
-subplot(2,2,1)
 j_1 = imadjustn(mri1);
 imshow(j_1, [])
 title('MRI Image 1 - Thresholding')
 
+
 ROI_1 = drawfreehand
 bw = createMask(ROI_1);
 imshow(bw)
-
-
-subplot(2,2,2)
-j_2 = imadjustn(mri2);
-imshow(j_2, [])
-title('MRI Image 2 - Thresholding')
-
-ROI_2 = drawfreehand
-bw2 = createMask(ROI_2);
-imshow(bw2)
-
-subplot(2,2,3)
-j_3 = imadjustn(mri3);
-imshow(j_3, [])
-title('MRI Image 2 - Thresholding')
-
-ROI_3 = drawfreehand
-bw3 = createMask(ROI_3);
-imshow(bw3)
-
-subplot(2,2,4)
-j_4 = imadjustn(mri4);
-imshow(j_4, [])
-title('MRI Image 2 - Thresholding')
-
-ROI_4 = drawfreehand
-bw4 = createMask(ROI_4);
-imshow(bw4)
 
 %% Finding area
 Area1 = nnz(bw)
