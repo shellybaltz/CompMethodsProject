fb9d099f16376e17c448617be9435cbcb51da3f0
shellybaltz <61434768+shellybaltz@users.noreply.github.com>
2020-04-22 22:50:07 -0600

    update

--- GUImrivolume.m
+++ GUImrivolume.m
@@ -188,29 +188,6 @@ function ROIthreshold_pushbutton_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 
 
-
-function toolboxcheck_textbox_Callback(hObject, eventdata, handles)
-% hObject    handle to toolboxcheck_textbox (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'String') returns contents of toolboxcheck_textbox as text
-%        str2double(get(hObject,'String')) returns contents of toolboxcheck_textbox as a double
-
-
-% --- Executes during object creation, after setting all properties.
-function toolboxcheck_textbox_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to toolboxcheck_textbox (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: edit controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
-
 % --- Executes on selection change in listbox1.
 function listbox1_Callback(hObject, eventdata, handles)
 % hObject    handle to listbox1 (see GCBO)
